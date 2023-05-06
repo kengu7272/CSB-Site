81d5231579b59540f2284171de59e7a32ab20399
kengu7272 <kengu72@gmail.com>
2023-05-06 03:26:23 -0400

    Update MainHeaderBar.tsx

--- src/Components/MainHeaderBar.tsx
+++ src/Components/MainHeaderBar.tsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 function MainHeaderBar() {
     return (
       <div className="flex flex-row items-center h-[8vh] border-b border-[#636363]">
-        <h2 className="ml-[1vw] absolute">CSB Enterprises</h2>
+        <h2 className="ml-[1vw] absolute text-[1.75vw]">CSB Enterprises</h2>
         <nav className="flex gap-[5vw] ml-auto mr-auto">
           <Link to="/">Home</Link>
           <Link to="/About">About</Link>
